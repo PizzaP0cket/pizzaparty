47c2e6803aa6410d6d02a20d769649b697a62da2
PizzaP0cket <andrew.buchanan8@hotmail.com>
2024-09-28 19:42:47 +1000

    Fix button

--- src/components/Main/Header/NavMenu/Profile.js
+++ src/components/Main/Header/NavMenu/Profile.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Image, Offcanvas } from "react-bootstrap";
+import { Image, Offcanvas, Button } from "react-bootstrap";
 import Authentication from '../../../../utils/auth';
 import QRCode from '../../../../utils/QRCode';
 
@@ -23,8 +23,8 @@ export default function Profile({ profileInfo }) {
                     {profileInfo.images && profileInfo.images.length > 0 ? (<Image key={`Image${0}`} src={profileInfo.images[1].url} alt="Profile" className="profile-avatar" />) : (<img src="https://via.placeholder.com/150" alt="Profile" className="profile-avatar" />)}
                     <div className="profile-info">
                         <h2 className="profile-name">{!profileInfo.display_name ? ('Please Sign in') : (profileInfo.display_name)}</h2>
-                        {!login ? (<button onClick={handleLogin}>Sign in</button>) : (<Authentication />)}
-                        {!code ? (<button onClick={handleQRCode}>QRCode</button>) : (<QRCode />)}
+                        {!login ? (<Button variant='success' size='sm' onClick={handleLogin}>Sign in</Button>) : (<Authentication />)}
+                        {!code ? (<Button variant='success' size='sm' onClick={handleQRCode}>QRCode</Button>) : (<QRCode />)}
                     </div>
                     <Offcanvas.Header closeButton>
                     </Offcanvas.Header>
